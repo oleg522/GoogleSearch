fef399fe894388eb68378d791956080fadb2fd66
Oleg Bakatin <on@messapps.com>
2020-02-16 12:28:22 +0300

    Initial Commit

---
+++ GoogleSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleSearch
+//
+//  Created by Oleg Bakatin on 16.02.2020.
+//  Copyright © 2020 Oleg Bakatin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
